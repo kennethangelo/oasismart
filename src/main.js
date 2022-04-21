27632901e3288a500ed931d3bca68b5058a082a4
Kenneth Angelo <tanjayakenneth@gmail.com>
2022-04-21 23:14:22 +0700

    First init

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Import CSS files
+// Automatically import it globally into the project
+// import './assets/styles/main.css'
+import './assets/styles/style.scss'
+
 createApp(App).use(router).mount('#app')
